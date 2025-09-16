0e2f6601373371925f5c496affdd5b3ee3b4f7c2
Jared Gilbert <jgilbert6867@gmail.com>
2025-09-15 20:19:42 -0400

    Functionality change:
    Refactor forgot password functionality to accept user email input and send reset link

--- TickrApp/app/forgotPassword.tsx
+++ TickrApp/app/forgotPassword.tsx
@@ -3,20 +3,29 @@ import React from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 import { auth } from '@/firebaseConfig'
 import { sendPasswordResetEmail } from 'firebase/auth';
+import { useRouter } from 'expo-router';
 
 export default function forgotPassword() {
+  const router = useRouter();
+  const [emailAddress, setEmailAddress] = React.useState('');
 
 
 
     //Change Password function
       const handleChangePassword = async () => {
         try{
-          if (auth.currentUser?.email) {
-            await sendPasswordResetEmail(auth, auth.currentUser.email);
-            Alert.alert("Password Reset", "A password reset email has been sent to your email address.");
-          } else{
-            Alert.alert("Error", "No email associated with this account.");
-          }
+          if (!emailAddress) {
+            Alert.alert("Error", "Please type a valid email address.");
+            return;
+          } 
+          
+          await sendPasswordResetEmail(auth, emailAddress.trim());
+          Alert.alert("Success", "Password reset sent. If an account exists with that email, you will receive a password reset link shortly.");
+          router.replace('/login');
+
+
+
+
         } catch (error) {
           console.error("Password reset error:", error);
           Alert.alert("Error", "Unable to send password reset email.");
@@ -31,15 +40,20 @@ export default function forgotPassword() {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
           <Text style={styles.title}>Reset your Password</Text>
-          <Text style={styles.title}>Enter the email address associated with your account, and we’ll send you a link to reset your password. Check your inbox (and spam folder) for the reset email. It may take up to 10 minutes to receive the reset link.</Text>
+          <Text style={styles.text}>Enter the email address associated with your account, and we’ll send you a link to reset your password. Check your inbox (and spam folder) for the reset email. It may take up to 10 minutes to receive the reset link.</Text>
     
           <TextInput
             style={styles.input}
             placeholder="Email Address"
             keyboardType="email-address"
             autoCapitalize="none"
-            value='emailAddress'
+            value={emailAddress}
+            onChangeText={setEmailAddress}
             autoCorrect={false} />  
+
+            <TouchableOpacity onPress={handleChangePassword} style={styles.submitButton}>
+              <Text style={styles.submitText}>Send Reset Link</Text>
+            </TouchableOpacity>
         </View>
     </TouchableWithoutFeedback>
   )
@@ -56,6 +70,11 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         textAlign: 'center',
     },
+    text: {
+        fontSize: 16,
+        marginBottom: 20,
+        textAlign: 'center',
+    }
     input: {
         flexDirection: "row",
         justifyContent: "space-between",
@@ -66,4 +85,18 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "#e5e7eb",
       },
+      submitButton: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 16,
+        backgroundColor: "#f9f9f9",
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: "#e5e7eb",
+      },
+      submitText: {
+        fontSize: 16,
+        color: "#374151",
+      }
 })

3ce21845b7922cdc429aed019c834ae3c659e884
Jared Gilbert <jgilbert6867@gmail.com>
2025-11-27 18:11:35 -0500

    fix: improve GoogleService-Info.plist handling in app.config.js

--- TickrApp/app.config.js
+++ TickrApp/app.config.js
@@ -4,9 +4,11 @@ import path from 'path';
 export default ({ config }) => {
   const plistPath = path.resolve(__dirname, 'GoogleService-Info.plist');
 
-  if (!fs.existsSync(plistPath) && process.env.GOOGLE_SERVICES_PLIST) {
-    console.log('Injecting GoogleService-Info.plist from EAS secret');
+  if (process.env.GOOGLE_SERVICES_PLIST) {
+    console.log('Writing GoogleService-Info.plist from EAS secret');
     fs.writeFileSync(plistPath, process.env.GOOGLE_SERVICES_PLIST, { encoding: 'utf8' });
+  } else {
+    console.warn('GOOGLE_SERVICES_PLIST environment variable not found!');
   }
 
   return {
